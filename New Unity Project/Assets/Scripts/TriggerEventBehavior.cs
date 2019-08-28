6c715a6f3607a0c390230ff2f7aeedbeb4123bae
Micah <hager.micah@gmail.com>
2019-08-28 13:46:24 -0600

    Float Data

---
+++ New Unity Project/Assets/Scripts/TriggerEventBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehavior : MonoBehaviour
+{
+
+    public UnityEvent TriggerEnterEvent;
+
+    private void OnTriggerEnter (Collider other) {
+        TriggerEnterEvent.Invoke();
+    }
+
+}
