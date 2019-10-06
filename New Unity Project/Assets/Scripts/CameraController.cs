61317b09cd0d5221ec2495673a4aa33928813ed7
Micah <hager.micah@gmail.com>
2019-10-06 16:28:36 -0600

    Camera Follow

---
+++ New Unity Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject targObj;
+
+    private float yOffset;
+    private float zOffset;
+
+    private Vector3 cameraPos;
+    private Vector3 targPos;
+
+    void Start()
+    {
+        yOffset = transform.position.y-targObj.transform.position.y;
+        zOffset = -transform.position.z-targObj.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        cameraPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        targPos = new Vector3(targObj.transform.position.x, targObj.transform.position.y+yOffset, targObj.transform.position.z-zOffset);
+
+        transform.Translate( targPos-cameraPos );
+    }
+}
